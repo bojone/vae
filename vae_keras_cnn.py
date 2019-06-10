080d90d52eedb216b50b9462596ede0e15f66e1b
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-06-10 14:43:03 +0800

    Create vae_keras_cnn.py

---
+++ vae_keras_cnn.py
@@ -0,0 +1,149 @@
+#! -*- coding: utf-8 -*-
+
+'''用Keras实现的VAE，CNN版本
+   目前只保证支持Tensorflow后端
+ #来自
+  https://github.com/keras-team/keras/blob/master/examples/variational_autoencoder_deconv.py
+'''
+
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+from keras.layers import Dense, Input
+from keras.layers import Conv2D, Flatten, Lambda
+from keras.layers import Reshape, Conv2DTranspose
+from keras.models import Model
+from keras import backend as K
+from keras.datasets import mnist
+
+
+batch_size = 100
+latent_dim = 2 # 隐变量取2维只是为了方便后面画图
+intermediate_dim = 256
+epochs = 50
+
+
+# 加载MNIST数据集
+(x_train, y_train_), (x_test, y_test_) = mnist.load_data()
+
+image_size = x_train.shape[1]
+x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
+x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+
+# 网络参数
+input_shape = (image_size, image_size, 1)
+batch_size = 128
+kernel_size = 3
+filters = 16
+latent_dim = 2
+epochs = 30
+
+x_in = Input(shape=input_shape)
+x = x_in
+
+for i in range(2):
+    filters *= 2
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               activation='relu',
+               strides=2,
+               padding='same')(x)
+
+# 备份当前shape，等下构建decoder的时候要用
+shape = K.int_shape(x)
+
+x = Flatten()(x)
+x = Dense(16, activation='relu')(x)
+# 算p(Z|X)的均值和方差
+z_mean = Dense(latent_dim)(x)
+z_log_var = Dense(latent_dim)(x)
+
+# 重参数技巧
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=K.shape(z_mean))
+    return z_mean + K.exp(z_log_var / 2) * epsilon
+
+# 重参数层，相当于给输入加入噪声
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+# 解码层，也就是生成器部分
+# 先搭建为一个独立的模型，然后再调用模型
+latent_inputs = Input(shape=(latent_dim,))
+x = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+for i in range(2):
+    x = Conv2DTranspose(filters=filters,
+                        kernel_size=kernel_size,
+                        activation='relu',
+                        strides=2,
+                        padding='same')(x)
+    filters //= 2
+
+outputs = Conv2DTranspose(filters=1,
+                          kernel_size=kernel_size,
+                          activation='sigmoid',
+                          padding='same')(x)
+
+# 搭建为一个独立的模型
+decoder = Model(latent_inputs, outputs, name='decoder')
+
+x_out = decoder(z)
+
+# 建立模型
+vae = Model(x_in, x_out)
+
+# xent_loss是重构loss，kl_loss是KL loss
+xent_loss = K.sum(K.binary_crossentropy(x_in, x_out), axis=[1, 2, 3])
+kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+vae_loss = K.mean(xent_loss + kl_loss)
+
+# add_loss是新增的方法，用于更灵活地添加各种loss
+vae.add_loss(vae_loss)
+vae.compile(optimizer='rmsprop')
+vae.summary()
+
+vae.fit(x_train,
+        shuffle=True,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=(x_test, None))
+
+
+# 构建encoder，然后观察各个数字在隐空间的分布
+encoder = Model(x_in, z_mean)
+
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+plt.figure(figsize=(6, 6))
+plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test_)
+plt.colorbar()
+plt.show()
+
+
+# 观察隐变量的两个维度变化是如何影响输出结果的
+n = 15  # figure with 15x15 digits
+digit_size = 28
+figure = np.zeros((digit_size * n, digit_size * n))
+
+#用正态分布的分位数来构建隐变量对
+grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
+grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+
+for i, yi in enumerate(grid_x):
+    for j, xi in enumerate(grid_y):
+        z_sample = np.array([[xi, yi]])
+        x_decoded = decoder.predict(z_sample)
+        digit = x_decoded[0].reshape(digit_size, digit_size)
+        figure[i * digit_size: (i + 1) * digit_size,
+               j * digit_size: (j + 1) * digit_size] = digit
+
+plt.figure(figsize=(10, 10))
+plt.imshow(figure, cmap='Greys_r')
+plt.show()
