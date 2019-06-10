5986f2733feb827b7a0786cfa8396e47fe371b72
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-06-10 14:20:55 +0800

    Update vae_keras.py

--- vae_keras.py
+++ vae_keras.py
@@ -16,7 +16,6 @@ from scipy.stats import norm
 from keras.layers import Input, Dense, Lambda
 from keras.models import Model
 from keras import backend as K
-from keras import metrics
 from keras.datasets import mnist
 
 
@@ -63,7 +62,7 @@ x_decoded_mean = decoder_mean(h_decoded)
 vae = Model(x, x_decoded_mean)
 
 # xent_loss是重构loss，kl_loss是KL loss
-xent_loss = original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
+xent_loss = original_dim * K.binary_crossentropy(x, x_decoded_mean)
 kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
 vae_loss = K.mean(xent_loss + kl_loss)
 
