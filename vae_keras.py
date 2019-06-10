ccec3d5e0e61334c27a7cd99c7a0c0383756cb0c
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-06-10 14:25:23 +0800

    Update vae_keras.py

--- vae_keras.py
+++ vae_keras.py
@@ -62,7 +62,7 @@ x_decoded_mean = decoder_mean(h_decoded)
 vae = Model(x, x_decoded_mean)
 
 # xent_loss是重构loss，kl_loss是KL loss
-xent_loss = original_dim * K.binary_crossentropy(x, x_decoded_mean)
+xent_loss = K.sum(K.binary_crossentropy(x, x_decoded_mean), axis=-1)
 kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
 vae_loss = K.mean(xent_loss + kl_loss)
 
